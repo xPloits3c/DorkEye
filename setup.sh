dc9d193e89ec8b35ac3c3ef15dc9c5e6171d93a4
I.C.W.T <153435050+xPloits3c@users.noreply.github.com>
2026-01-31 18:13:20 +0100

    setup.sh

--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@ NC='\033[0m' # No Color
 
 echo -e "${GREEN}"
 echo "  ___  "
-echo " __H__    DorkEye v3.2 Setup"
+echo " __H__    DorkEye v3.8 Setup"
 echo "  [,]  "
 echo "  [)]  "
 echo "  [;]    Setting up environment..."
